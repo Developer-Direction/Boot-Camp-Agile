01734526119ba026cdf95eb35e79ea0fe8306a8f
Stewart <saxup@ucsc.edu>
2022-08-16 18:54:33 -0700

    fixed loading bar & added system status functionality
    fixed loading bar issues and implemented get server status

--- bot.py
+++ bot.py
@@ -5,11 +5,11 @@ import urllib.parse
 import hashlib
 import hmac
 import base64
-from tqdm.auto import tqdm
+from tqdm import tqdm
 import Kraken_Request as kr
 
 
-
+#npn
 # print string to comand line welcome to your Python Trading Bot
 print("Welcome to your Python Trading Bot")
 
@@ -34,6 +34,7 @@ while run:
     print("Cancel Order\n")
     print("Fund Account\n")
     print("Change Account\n")
+    print("Get System Status\n")
     print("Exit\n")
     
     # get user input for action
@@ -61,6 +62,15 @@ while run:
     if action == "exit":
         run = False
         # if user action is not listed then print error message
+    
+    if action == "get system status":
+        # get system status
+        # get_system_status()
+        resp = requests.get('https://api.kraken.com/0/public/SystemStatus')
+        print("System Status is:")
+        print(resp.json())
+        # sleep for 1 second to allow user to read output
+        time.sleep(1)
     
     # consider rewriting to check from a listy instead of if else and a ton of conditionals
     elif action != "get account balance" and action != "get account history" and action != "get open orders" and action != "cancel order" and action != "fund account" and action != "change account":
