038cc0c95f3bef3381c70cba644e6f2ae2769f52
Michael Campagnoli <michael.campagnoli@protonmail.com>
2022-08-22 14:45:22 -0400

    Add Commit

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # create a command line interface for the bot that runs and says "wwelcome to your Python Trading Bot"
+from pyclbr import Function
 import time
 import requests
 import urllib.parse
@@ -27,6 +28,7 @@ while run:
     print("Get Account History\n")
     print("Get Open Orders\n")
     print("Cancel Order\n")
+    print("Fund Menu\n")
     print("Fund Account\n")
     print("Change Account\n")
     print("Exit\n")
@@ -37,7 +39,58 @@ while run:
     #convert action to lower case
     action = action.lower()
 
-    #if user input is get account balance
+    #Fund Menu option
+    def fund_menu():
+        print("[1] Fund Method")
+        print("[2] Funding Address")
+        print("exit")
+
+        fund_menu()
+
+        fundOption = int(input("Enter your method: "))
+
+        # Options for fund mennu
+        while fundOption != 0:
+            if fundOption == 1:
+                print("Method has been called.")
+    
+            if fundOption != 0:
+                print("Enter your address: ")
+    
+            else: 
+                print("Invalid address")
+
+        print()
+        fund_menu()
+
+        fundOption = int(input("Enter your address: "))
+
+    
+    # Fund account
+    action = input("Fund Account: ")
+
+    #if user input is fund account
+    if action == "Fund Account":
+        fund_Account()
+
+    # function for funding account address and funding method
+    def fund_Account():
+        data = '/0/private/DepositAddresses', {
+        "nonce": str(int(1000*time.time())),
+        "asset": "XBT",
+        "method": "Bitcoin",
+        "new": True
+        }, api_key, api_secret
+
+        data = '/0/private/DepositMethods', {
+        "nonce": str(int(1000*time.time())),
+        "asset": "XBT"
+        }, api_key, api_sec
+
+        # kraken request
+        kraken_request()
+
+     #if user input is get account balance
     if action == "get account balance":
         #get account balance
         #get_account_balance()
@@ -50,6 +103,8 @@ while run:
     if action == "change account":
         api_key = input("Please enter your API Key: ")
         api_secret = input("Please enter your API Secret: ")
+
+    
     
     #if user action is exit then exit loop
     if action == "exit":
